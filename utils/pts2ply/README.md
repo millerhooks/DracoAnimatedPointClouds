c8ca7d41aa88498cca2c4dcfd1c5cac6d43f8886
Miller Hooks <miller@nicehou.se>
2017-10-26 03:26:02 -0500

    added pts2ply sources and cmake files, as well as an OSX binary

---
+++ utils/pts2ply/README.md
@@ -0,0 +1,1 @@
+# Tool for converting PTS to PLY based off https://github.com/dmtsui/PTS-2-PLY-Converter
---
+++ utils/pts2ply/main.cpp
@@ -0,0 +1,126 @@
+/*
+PtsToPlyConverter Program
+Converts a PTS or TXT based PTS file into a PLY file for use in Math
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *strrev(char *str)
+{
+    char *p1, *p2;
+
+    if (! str || ! *str)
+        return str;
+    for (p1 = str, p2 = str + strlen(str) - 1; p2 > p1; ++p1, --p2)
+    {
+        *p1 ^= *p2;
+        *p2 ^= *p1;
+        *p1 ^= *p2;
+    }
+    return str;
+}
+
+void getExtension(char*, char*);
+
+int main(int argc, const char* argv[])
+{
+    if (argc <= 1){
+        printf("Please specify both PTS in and PLY out.");
+        return 0;
+    }
+
+	unsigned int lineCount = 0;
+	int byte, i;
+	char filename[255];
+	char newFilename[255];
+	char ext[25];
+	FILE *f_in;
+	FILE *f_out;
+
+	strcpy(filename, argv[1]);
+	strcpy(newFilename, filename);
+	getExtension(ext, filename);
+
+	if (strcmp(ext, "pts") == 0 || strcmp(ext, "txt") == 0 )
+	{
+
+
+		newFilename[strlen(newFilename) - 4] = '\0';
+		f_in = fopen(filename, "r");
+		strcat(newFilename, ".ply");
+		f_out = fopen(newFilename, "w");
+/**************************************************************************/
+printf("Conversion Started...\n\n");
+/**************************************************************************/
+//this will get a line count in the file.
+//We will need this to figure out how many points are in the point cloud
+		for (i = 0; (byte = getc(f_in)) != EOF; i++)
+		{
+			if(byte == '\n')
+				lineCount++;
+		}
+/**************************************************************************/
+
+/**************************************************************************/
+//We can now write the header for the Ply file with the correct
+//amount of vertex count
+
+fprintf(f_out, "ply\n");
+fprintf(f_out, "format ascii 1.0\n");
+fprintf(f_out, "comment author: Dan Tsui\n");
+fprintf(f_out, "comment object: Point Cloud\n");
+//lineCount minus the header in the pts file gives us the vertex count
+fprintf(f_out, "element vertex %d\n", lineCount-4);
+fprintf(f_out, "property float x\n");
+fprintf(f_out, "property float y\n");
+fprintf(f_out, "property float z\n");
+fprintf(f_out, "property uchar red\n");
+fprintf(f_out, "property uchar green\n");
+fprintf(f_out, "property uchar blue\n");
+fprintf(f_out, "end_header\n");
+
+/**************************************************************************/
+	lineCount = 0; //resets line counter
+	rewind(f_in); //reset cursor pointer to the beginning
+		for (i = 0; (byte = getc(f_in)) != EOF; i++)
+		{
+			if(byte == '\n')
+				lineCount++;
+			if(lineCount > 3) //starts writing the points after the cursor has passed the header
+			{
+				if(byte != ',') //This if else statement takes out the ',' deliminator
+					fputc(byte, f_out);
+				else
+					fputc(' ',f_out);
+			}
+		}
+
+/**************************************************************************/
+printf("Conversion Completed.");//prompts the user that the process has completed
+/**************************************************************************/
+	}
+	else
+	{
+		//file is not supported
+		printf("Error: File is not a .txt or .pts point cloud file.");
+
+	}
+	fclose(f_in);
+	fclose(f_out);
+	return 0;
+}
+
+void getExtension(char* ext, char* file)
+{
+	int i, j = 0;
+	for (i = strlen(file) - 1; i >= 0; i--)
+	{
+		if(file[i] == '.')
+			break;
+		ext[j] = file[i];
+		j++;
+	}
+	ext[j] = '\x0';
+	strrev(ext);
+}
