8d0ff89a28498055fa3c1a04af82a8c537b129cc
Miller Hooks <miller@nicehou.se>
2017-10-26 04:21:33 -0500

    script converts pts to drc

--- scripts/pts2ply_dir.sh
+++ scripts/pts2ply_dir.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-PTS2PLY=../utils/pts2ply/bin/osx/pts2ply
-SOURCE=../example_sources/AnimatedPointClouds/Brekel/BrekelKinect_capture_29_10_2016_18_09_41_Leica_PTS/*
+PTS2PLY=../utils/pts2ply/bin/osx/pts2ply_nocolor
+SOURCE=../example_sources/AnimatedPointClouds/Brekel/BrekelKinect_capture_29_10_2016_18_09_41_Leica_PTS/
 
 ls $SOURCE
-for file in $SOURCE
+for file in $SOURCE*
 do
   echo $($PTS2PLY "$file")
   echo -e "\n"
+  mv $SOURCE*.ply $SOURCE../converted/PLY
 done
+
+for file in $SOURCE../converted/PLY/*
+do
+  echo $(draco_encoder -i "$file")
+  echo -e "\n"
+  mv $SOURCE../converted/PLY/*.drc $SOURCE../converted/DRC
+done
+
+
